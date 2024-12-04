5de661a10bd6038f4147e36e41879596aaa4f070
ismailmaaitat <65847371+smorayzmaaitat@users.noreply.github.com>
2024-12-04 13:08:20 +0100

    Update user.vue

--- resources/js/Pages/user.vue
+++ resources/js/Pages/user.vue
@@ -34,6 +34,9 @@ export default {
     }
   },
   methods: {
+    deleteTicket(ticket) {
+      this.$inertia.post(`/deleteticket/${ticket.id}`);
+    },
     editTicket(ticket) {
       this.$inertia.visit(`/edittickets/${ticket.id}`);
     },
@@ -79,7 +82,7 @@ export default {
                 </div>
                 <div class="flex justify-between">
                   <button 
-                    class="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-300" 
+                    class="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-300 mr-2 " 
                     @click="editTicket(ticket)"
                   >
                     Edit Ticket
@@ -90,6 +93,14 @@ export default {
                   >
                     View Ticket Status
                   </button>
+                  <button 
+  @click="deleteTicket(ticket)"
+  class="text-red-500 hover:text-red-700 hover:bg-red-50 p-2 rounded-full transition-all duration-300 group"
+>
+  <svg class="w-5 h-5 group-hover:scale-110 transition-transform" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+  </svg>
+</button>
                 </div>
               </div>
             </div>
